c48c14d3627a9790fe58205eb12cbb0ea88544bf
Stevil Knevil <stevilknevil@hotmail.co.uk>
2017-09-14 16:43:27 +0100

    Made it find the correct emails

--- DomTurnMgr/Program.cs
+++ DomTurnMgr/Program.cs
@@ -8,13 +8,9 @@ using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 
 namespace DomTurnMgr
@@ -44,9 +40,6 @@ namespace DomTurnMgr
             new FileDataStore(credPath, true)).Result;
         Console.WriteLine("Credential file saved to: " + credPath);
 
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
       }
 
       // Create Gmail API service.
@@ -57,24 +50,85 @@ namespace DomTurnMgr
       });
 
       // Define parameters of request.
-      UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+      UsersResource.MessagesResource.ListRequest request = service.Users.Messages.List("me");
+      request.Q = "from:turns@llamaserver.net";
+      request.Q = " ";
+      request.Q = "subject:davrodmomma";
 
       // List labels.
-      IList<Google.Apis.Gmail.v1.Data.Label> labels = request.Execute().Labels;
-      Console.WriteLine("Labels:");
-      if (labels != null && labels.Count > 0)
+      IList<Google.Apis.Gmail.v1.Data.Message> msgs = request.Execute().Messages;
+      Console.WriteLine("msgs:");
+      if (msgs != null && msgs.Count > 0)
       {
-        foreach (var labelItem in labels)
+        foreach (var msg in msgs)
         {
-          Console.WriteLine("{0}", labelItem.Name);
+          MessagePart payload = service.Users.Messages.Get("me", msg.Id).Execute().Payload;
+          if (payload == null || payload.Headers == null)
+            continue;
+
+          bool foundSubject = false;
+          bool foundFrom = false;
+          string subject;
+          string from;
+          // See if this message has the correct headers that we're interested in
+          foreach (var header in payload.Headers)
+          {
+
+            if (header.Name.Equals("Subject") && header.Value.Contains("New turn file: davrodmomma"))
+            {
+              subject = header.Value;
+              foundSubject = true;
+            }
+            if (header.Name.Equals("From") && header.Value.Contains("turns@llamaserver.net"))
+            {
+              from = header.Value;
+              foundFrom = true;
+            }
+            if (foundSubject && foundFrom)
+            {
+              Console.WriteLine("{0}", msg.Id);
+              break;
+            }
+          }
         }
       }
       else
       {
         Console.WriteLine("No labels found.");
       }
+
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+
       Console.Read();
 
+
     }
+
+    /*
+    public static void getAttachments(GmailService service, String userId, String messageId)
+    {
+      Google.Apis.Gmail.v1.Data.Message message = service.Users.Messages.Get(userId, messageId).Execute();
+      List<MessagePart> parts = message.Payload.Parts;
+      foreach (MessagePart part in parts)
+      {
+        if (part.Filename != null && part.Filename.Length > 0)
+        {
+          String filename = part.Filename;
+          String attId = part.Body.AttachmentId;
+          MessagePartBody attachPart = service.Users.Messages().attachment().
+              get(userId, messageId, attId).execute();
+
+          Base64 base64Url = new Base64(true);
+          byte[] fileByteArray = base64url.decodeBase64(attachPart.Data);
+          FileStream fileOutFile =
+              new FileStream("directory_to_store_attachments" + filename, FileMode.OpenOrCreate, FileAccess.Write);
+          fileOutFile.WriteByte(fileByteArray);
+          fileOutFile.Close();
+        }
+      }
+      }
+    */
   }
 }
