ead244992baabc25b7032f732b713e91742ad8f2
Stevil Knevil <stevilknevil@hotmail.co.uk>
2017-09-14 12:02:49 +0100

    Shifted files and initial implementation

---
+++ DomTurnMgr/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace DomTurnMgr
+{
+  class Program
+  {
+    // If modifying these scopes, delete your previously saved credentials
+    // at ~/.credentials/gmail-dotnet-quickstart.json
+    static string[] Scopes = { GmailService.Scope.GmailReadonly };
+    static string ApplicationName = "Domionions Turn Manager";
+
+    static void Main(string[] args)
+    {
+      UserCredential credential;
+      using (var stream =
+          new FileStream(@"client_secret.json", FileMode.Open, FileAccess.Read))
+      {
+        string credPath = System.Environment.GetFolderPath(
+            System.Environment.SpecialFolder.Personal);
+        credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            Scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath, true)).Result;
+        Console.WriteLine("Credential file saved to: " + credPath);
+
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
+      }
+
+      // Create Gmail API service.
+      var service = new GmailService(new BaseClientService.Initializer()
+      {
+        HttpClientInitializer = credential,
+        ApplicationName = ApplicationName,
+      });
+
+      // Define parameters of request.
+      UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+      // List labels.
+      IList<Google.Apis.Gmail.v1.Data.Label> labels = request.Execute().Labels;
+      Console.WriteLine("Labels:");
+      if (labels != null && labels.Count > 0)
+      {
+        foreach (var labelItem in labels)
+        {
+          Console.WriteLine("{0}", labelItem.Name);
+        }
+      }
+      else
+      {
+        Console.WriteLine("No labels found.");
+      }
+      Console.Read();
+
+    }
+  }
+}
