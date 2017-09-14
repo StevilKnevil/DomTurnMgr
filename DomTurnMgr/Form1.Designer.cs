13c1148f68c78b9e695add9f2c8af08ea8ccbc1d
Stevil Knevil <stevilknevil@hotmail.co.uk>
2017-09-14 18:04:11 +0100

    1st pass UI

--- DomTurnMgr/Form1.Designer.cs
+++ DomTurnMgr/Form1.Designer.cs
@@ -28,12 +28,110 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(276, 24);
+      this.menuStrip1.TabIndex = 3;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // editToolStripMenuItem
+      // 
+      this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.preferencesToolStripMenuItem});
+      this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+      this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+      this.editToolStripMenuItem.Text = "Edit";
+      // 
+      // preferencesToolStripMenuItem
+      // 
+      this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+      this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+      this.preferencesToolStripMenuItem.Text = "Preferences";
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button2.Location = new System.Drawing.Point(189, 83);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 50);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Launch Dominions";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // button3
+      // 
+      this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button3.Location = new System.Drawing.Point(189, 139);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 50);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "Send Latest";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.Location = new System.Drawing.Point(189, 27);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 50);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Get Latest";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // listView1
+      // 
+      this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.listView1.Location = new System.Drawing.Point(12, 27);
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(171, 199);
+      this.listView1.TabIndex = 4;
+      this.listView1.UseCompatibleStateImageBehavior = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(276, 238);
+      this.Controls.Add(this.listView1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ListView listView1;
   }
 }
 
--- DomTurnMgr/Program.cs
+++ DomTurnMgr/Program.cs
@@ -15,10 +15,23 @@ using System.Threading;
 
 namespace DomTurnMgr
 {
+  /*
+   * ListView witch checks - shows all turns
+   *   colour to differentiate between current and old - grey can be ignored
+   *   ticked ~= ticked state in Dom 4
+   *   so a ticked item has has a .2h file sent back to the server for that go
+   *   Unticked means that only a trn file exists and the .2h file hasn't been sent (according to gmail)
+   *   
+   *   preference for server email address, game name and and advanced 'search string'
+   *   Preference for Dom4 exe & user data sir.
+   *   set defaults on startup and show the prefs dialog on first startup.
+   *   
+   *   Option to delete old turn emails from gmail
+   */
   class Program
   {
     // If modifying these scopes, delete your previously saved credentials
-    // at ~/.credentials/gmail-dotnet-quickstart.json
+    // at ~/.credentials/skapps-domTurnManager.json
     static string[] Scopes = { GmailService.Scope.GmailReadonly };
     static string ApplicationName = "Domionions Turn Manager";
 
@@ -30,7 +43,7 @@ namespace DomTurnMgr
       {
         string credPath = System.Environment.GetFolderPath(
             System.Environment.SpecialFolder.Personal);
-        credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+        credPath = Path.Combine(credPath, ".credentials/skapps-domTurnManager.json");
 
         credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
             GoogleClientSecrets.Load(stream).Secrets,
