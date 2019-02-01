ef509fb6ab81d6c20db479bd4be350ac02cab85b
Stevil <stevilknevil@hotmail.co.uk>
2019-02-01 10:23:10 +0000

    Programatically add the game control

--- DomTurnMgr/GameControl.cs
+++ DomTurnMgr/GameControl.cs
@@ -39,6 +39,10 @@ namespace DomTurnMgr
     public GameControl()
     {
       InitializeComponent();
+      AllowDrop = true;
+      DragEnter += GameControl_DragEnter;
+      DragDrop += GameControl_DragDrop;
+
       Program.TurnManager.TurnsChanged += TurnManager_TurnsChanged;
     }
 
