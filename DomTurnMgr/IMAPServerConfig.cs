41a839903981958ed3b70cc0f37cfb8f0c717857
Stevil Knevil <stevilknevil@hotmail.co.uk>
2019-02-03 12:48:31 +0000

    Don't save password in plaintext

--- DomTurnMgr/IMAPServerConfig.cs
+++ DomTurnMgr/IMAPServerConfig.cs
@@ -2,45 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Security;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace DomTurnMgr
 {
-  public class IMAPServerConfig
+  [Serializable]
+  public class IMAPServerConfig : IXmlSerializable
   {
+    public ICredentials Credentials => new NetworkCredential(Username, Password);
+    public string Address;
+    public int Port;
+    public string Username;
+    public string Password;
+
     public IMAPServerConfig()
     {
     }
 
-    public IMAPServerConfig(string address, int port, string username, SecureString password)
+    public IMAPServerConfig(string address, int port, string username, string password)
     {
       Address = address;
       Port = port;
       Username = username;
-      // todo: dispose!
-      Password = password.Copy();
+      Password = password;
     }
 
-    public string Address { get; }
-    public int Port { get; }
-    public string Username;
-    public SecureString Password;
-    public ICredentials Credentials => new NetworkCredential(Username, Password);
+    #region IXmlSerializable
+    public System.Xml.Schema.XmlSchema GetSchema()
+    {
+      return null;
+    }
 
+    public void ReadXml(System.Xml.XmlReader reader)
+    {
+      reader.Read();
 
-    public static string Protect(SecureString stringToEncrypt, string optionalEntropy, DataProtectionScope scope)
+      while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+      {
+        string nodeName = reader.Name;
+        var f = this.GetType().GetField(nodeName);
+        XmlSerializer valueSerializer =
+                          new XmlSerializer(f.FieldType);
+
+        reader.ReadStartElement(nodeName);
+        object tempValue = null;
+        tempValue = valueSerializer.Deserialize(reader);
+        f.SetValue(this, tempValue);
+
+        reader.ReadEndElement();
+      }
+
+      Unprotect();
+    }
+
+    public void WriteXml(System.Xml.XmlWriter writer)
+    {
+      Protect();
+
+      foreach (var f in GetType().GetFields())
+      {
+        string fieldName = f.Name;
+        XmlSerializer valueSerializer = new XmlSerializer(f.FieldType);
+        writer.WriteStartElement(fieldName);
+        valueSerializer.Serialize(writer, f.GetValue(this));
+        writer.WriteEndElement();
+      }
+
+      Unprotect();
+    }
+    #endregion IXmlSerializable
+
+    #region Security
+    private void Protect()
+    {
+      Password = ProtectString(Password, null, DataProtectionScope.CurrentUser);
+    }
+
+    private void Unprotect()
+    {
+      Password = UnprotectString(Password, null, DataProtectionScope.CurrentUser);
+    }
+
+    private static string ProtectString(string stringToEncrypt, string optionalEntropy, DataProtectionScope scope)
     {
       byte[] encryptedData = System.Security.Cryptography.ProtectedData.Protect(
-          System.Text.Encoding.Unicode.GetBytes(ToInsecureString(stringToEncrypt)),
+          System.Text.Encoding.Unicode.GetBytes(stringToEncrypt),
            optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null,
           System.Security.Cryptography.DataProtectionScope.CurrentUser);
       return Convert.ToBase64String(encryptedData);
     }
 
-    public static SecureString Unprotect(string encryptedData, string optionalEntropy, DataProtectionScope scope)
+    private static string UnprotectString(string encryptedData, string optionalEntropy, DataProtectionScope scope)
     {
       try
       {
@@ -48,38 +106,13 @@ namespace DomTurnMgr
             Convert.FromBase64String(encryptedData),
              optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null,
             System.Security.Cryptography.DataProtectionScope.CurrentUser);
-        return ToSecureString(System.Text.Encoding.Unicode.GetString(decryptedData));
+        return System.Text.Encoding.Unicode.GetString(decryptedData);
       }
       catch
       {
-        return new SecureString();
+        return "";
       }
     }
-
-    public static SecureString ToSecureString(string input)
-    {
-      SecureString secure = new SecureString();
-      foreach (char c in input)
-      {
-        secure.AppendChar(c);
-      }
-      secure.MakeReadOnly();
-      return secure;
-    }
-
-    public static string ToInsecureString(SecureString input)
-    {
-      string returnValue = string.Empty;
-      IntPtr ptr = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(input);
-      try
-      {
-        returnValue = System.Runtime.InteropServices.Marshal.PtrToStringBSTR(ptr);
-      }
-      finally
-      {
-        System.Runtime.InteropServices.Marshal.ZeroFreeBSTR(ptr);
-      }
-      return returnValue;
-    }
+    #endregion Security
   }
 }
