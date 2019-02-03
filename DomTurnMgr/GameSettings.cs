c92af257d2a2e23244305ddac44300116e8f739f
Stevil Knevil <stevilknevil@hotmail.co.uk>
2019-02-03 19:12:24 +0000

    Add game settings class

---
+++ DomTurnMgr/GameSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomTurnMgr
+{
+  class GameSettings
+  {
+    public GameSettings() {}
+
+    string Name;
+    struct MailSearchQuery
+    {
+      string SubjectMatch;
+      string SenderMatch;
+    }
+
+    IMailServerConfig MailServerConfig;
+
+    struct GameServerConfig
+    {
+      Uri GameServerAddress;
+      string adminPassword;
+    }
+  }
+}
