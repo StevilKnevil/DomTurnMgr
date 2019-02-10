a0feac9f862c78420bb20f5b25dbef7abf2dde98
Stevil Knevil <stevilknevil@hotmail.co.uk>
2019-02-10 11:38:58 +0000

    Added a utility for sending mail

---
+++ DomTurnMgr/SMTPMailSender.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomTurnMgr
+{
+  class SMTPMailSender
+  {
+    public static void SendMail(MailServerConfig msg, string subject, string body, string[] attachmentPaths)
+    {
+      var message = new MimeMessage();
+
+      message.From.Add(new MailboxAddress("Joey Tribbiani", "steeveeet@gmail.com"));
+      message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "steeveeet@hotmail.com"));
+      message.Subject = subject;
+
+      var builder = new BodyBuilder();
+
+      // Set the plain-text version of the message text
+      builder.TextBody = body;
+      foreach (var f in attachmentPaths)
+      {
+        builder.Attachments.Add(f);
+      }
+
+      // Now we just need to set the message body and we're done
+      message.Body = builder.ToMessageBody();
+
+      using (var client = new SmtpClient())
+      {
+        client.Connect(msg.SMTPAddress, msg.SMTPPort);
+
+        ////Note: only needed if the SMTP server requires authentication
+        client.Authenticate(msg.Username, msg.Password);
+
+        client.Send(message);
+        client.Disconnect(true);
+      }
+    }
+  }
+}
