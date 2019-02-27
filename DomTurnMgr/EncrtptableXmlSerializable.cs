442d4d8f65962a554a90c0e8a42230ad0f325749
Stevil <stevilknevil@hotmail.co.uk>
2019-02-27 21:19:57 +0000

    Pulled out the serialisation code into it's own class

---
+++ DomTurnMgr/EncrtptableXmlSerializable.cs
@@ -0,0 +1,66 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DomTurnMgr
+{
+  [Serializable]
+  public class EncryptableXmlSerializable : IXmlSerializable
+  {
+    #region IXmlSerializable
+    public System.Xml.Schema.XmlSchema GetSchema()
+    {
+      return null;
+    }
+
+    public void ReadXml(System.Xml.XmlReader reader)
+    {
+      reader.Read();
+
+      while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+      {
+        string nodeName = reader.Name;
+        var f = this.GetType().GetProperty(nodeName);
+        XmlSerializer valueSerializer =
+                          new XmlSerializer(f.PropertyType);
+
+        reader.ReadStartElement(nodeName);
+        object tempValue = null;
+        tempValue = valueSerializer.Deserialize(reader);
+        if (Attribute.IsDefined(f, typeof(XmlEncryptAttribute)))
+        {
+          tempValue = XmlEncryptAttribute.Unprotect(tempValue as string);
+        }
+
+        f.SetValue(this, tempValue);
+
+        reader.ReadEndElement();
+      }
+    }
+
+    public void WriteXml(System.Xml.XmlWriter writer)
+    {
+      foreach (var f in GetType().GetProperties())
+      {
+        if (!Attribute.IsDefined(f, typeof(XmlIgnoreAttribute)))
+        {
+          string fieldName = f.Name;
+          XmlSerializer valueSerializer = new XmlSerializer(f.PropertyType);
+          writer.WriteStartElement(fieldName);
+          object s = f.GetValue(this);
+          if (Attribute.IsDefined(f, typeof(XmlEncryptAttribute)))
+          {
+            s = XmlEncryptAttribute.Protect(s as string);
+          }
+          valueSerializer.Serialize(writer, s);
+          writer.WriteEndElement();
+        }
+      }
+    }
+    #endregion IXmlSerializable
+  }
+}
--- DomTurnMgr/MailServerConfig.cs
+++ DomTurnMgr/MailServerConfig.cs
@@ -8,8 +8,7 @@ using System.Xml.Serialization;
 
 namespace DomTurnMgr
 {
-  [Serializable]
-  public class MailServerConfig : IXmlSerializable
+  public class MailServerConfig : EncryptableXmlSerializable
   {
     public static Dictionary<string, MailServerConfig> MailServerConfigs = new Dictionary<string, MailServerConfig>();
     [XmlIgnore]
@@ -93,57 +92,5 @@ namespace DomTurnMgr
 
       return Status.OK;
     }
-
-    #region IXmlSerializable
-    public System.Xml.Schema.XmlSchema GetSchema()
-    {
-      return null;
-    }
-
-    public void ReadXml(System.Xml.XmlReader reader)
-    {
-      reader.Read();
-
-      while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
-      {
-        string nodeName = reader.Name;
-        var f = this.GetType().GetProperty(nodeName);
-        XmlSerializer valueSerializer =
-                          new XmlSerializer(f.PropertyType);
-
-        reader.ReadStartElement(nodeName);
-        object tempValue = null;
-        tempValue = valueSerializer.Deserialize(reader);
-        if (Attribute.IsDefined(f, typeof(XmlEncryptAttribute)))
-        {
-          tempValue = XmlEncryptAttribute.Unprotect(tempValue as string);
-        }
-
-        f.SetValue(this, tempValue);
-
-        reader.ReadEndElement();
-      }
-    }
-
-    public void WriteXml(System.Xml.XmlWriter writer)
-    {
-      foreach (var f in GetType().GetProperties())
-      {
-        if (!Attribute.IsDefined(f, typeof(XmlIgnoreAttribute)))
-        {
-          string fieldName = f.Name;
-          XmlSerializer valueSerializer = new XmlSerializer(f.PropertyType);
-          writer.WriteStartElement(fieldName);
-          object s = f.GetValue(this);
-          if (Attribute.IsDefined(f, typeof(XmlEncryptAttribute)))
-          {
-            s = XmlEncryptAttribute.Protect(s as string);
-          }
-          valueSerializer.Serialize(writer, s);
-          writer.WriteEndElement();
-        }
-      }
-    }
-    #endregion IXmlSerializable
   }
 }
